e0f2e12463aa9ff2ddd140c1e26a269eaf37e224
Lars Möllendorf <lars@moellendorf.eu>
2022-01-30 23:41:40 +0100

    examples: Auto format

--- examples/test_lm_ranger/test_lm_ranger.ino
+++ examples/test_lm_ranger/test_lm_ranger.ino
@@ -3,17 +3,19 @@
 
 Ranger ranger(SLOT_L, SLOT_R);
 
-void setup() {
+void setup()
+{
 }
 
-void loop() {
-  pinMode(45, OUTPUT);
-  digitalWrite(45, HIGH);
-  delay(250);
-  pinMode(45, OUTPUT);
-  digitalWrite(45, LOW);
-  delay(750);
+void loop()
+{
+	pinMode(45, OUTPUT);
+	digitalWrite(45, HIGH);
+	delay(250);
+	pinMode(45, OUTPUT);
+	digitalWrite(45, LOW);
+	delay(750);
 
-  ranger.MoveTo(6000, 200);
-  ranger.StartRainbow();
+	ranger.MoveTo(6000, 200);
+	ranger.StartRainbow();
 }
