26dda57beb11b52caab2a0744f825f996640c36f
Lars Möllendorf <lars@moellendorf.eu>
2022-02-03 19:56:50 +0100

    examples: remove heart beat from ranger example

--- examples/test_lm_ranger/test_lm_ranger.ino
+++ examples/test_lm_ranger/test_lm_ranger.ino
@@ -11,12 +11,5 @@ void setup()
 
 void loop()
 {
-	pinMode(45, OUTPUT);
-	digitalWrite(45, HIGH);
-	delay(250);
-	pinMode(45, OUTPUT);
-	digitalWrite(45, LOW);
-	delay(750);
-
 	ranger.MoveTo(6000, 200);
 }
