10744045ea062ca246ef560f09cadf6ee0b9c95e
Lars Möllendorf <lars@moellendorf.eu>
2022-02-06 23:50:33 +0100

    examples: add crush the flag parameter (kind of)

--- examples/test_lm_encoder_on_board_motor/test_lm_encoder_on_board_motor.ino
+++ examples/test_lm_encoder_on_board_motor/test_lm_encoder_on_board_motor.ino
@@ -19,14 +19,34 @@ void HeartBeat(void)
 
 void loop()
 {
+	int path1 = 4000;
+	int turn90 = 410;
+	int path2 = 750;
+	int path3 = 2600;
+	int path4 = 780;
 
 	LedRing::StopRainbow();
 	//Move forward and block until position is reached
-	motor_l.RotateTo(360, 200);
-	motor_r.RotateTo(-360, 200, true);
+	motor_l.RotateTo(path1, 200, false, true);
+	motor_r.RotateTo(-path1, 200, true, true);
 	//Rotate and block until position is reached
-	motor_l.RotateTo(360, 200, false);
-	motor_r.RotateTo(360, 200, true);
+	motor_l.RotateTo(-turn90, 200, false, true);
+	motor_r.RotateTo(-turn90, 200, true, true);
+	//Move forward and block until position is reached
+	motor_l.RotateTo(path2, 200, false, true);
+	motor_r.RotateTo(-path2, 200, true, true);
+	//Rotate and block until position is reached
+	motor_l.RotateTo(-turn90, 200, false, true);
+	motor_r.RotateTo(-turn90, 200, true, true);
+	//Move forward and block until position is reached
+	motor_l.RotateTo(path3, 200, false, true);
+	motor_r.RotateTo(-path3, 200, true, true);
+	//Rotate and block until position is reached
+	motor_l.RotateTo(turn90, 200, false, true);
+	motor_r.RotateTo(turn90, 200, true, true);
+	//Move forward and block until position is reached
+	motor_l.RotateTo(path4, 200, false, true);
+	motor_r.RotateTo(-path4, 200, true, true);
 
 	HeartBeat();
 
