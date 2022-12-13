c294c2b26da023b8ef23a5b6d44e01306d2f0840
Lars Möllendorf <lars@moellendorf.eu>
2022-12-13 21:41:03 +0100

    Introduce	quadratic equation to dim RGB LED ring

--- examples/rgb_led_ring2/rgb_led_ring2.ino
+++ examples/rgb_led_ring2/rgb_led_ring2.ino
@@ -7,13 +7,21 @@ void setup()
 	led_ring.setpin(44);
 }
 
+uint8_t dim(double n_led, double led)
+{
+	return 255. * (-n_led * pow(led, 2) + pow(n_led,
+			2) * led) / (-n_led * pow(n_led / 2., 2) + pow(n_led, 2) * n_led / 2.);
+}
+
 void loop()
 {
-	for (int i = 0; i < 12; i++) {
-		led_ring.setColor(0, 0, 0, 0);
+	int n_led = 12;
 
-		for (int j = 0; j < 5; j++)
-			led_ring.setColor(((i + j) % 12) + 1, 255, 0, 0) ;
+	for (int i = 0; i < 12; i++) {
+		led_ring.setColor(0, 0, 0, 0) ;
+
+		for (int j = 0; j < n_led; j++)
+			led_ring.setColor(((i + j) % 12) + 1, dim(n_led, j), 0, 0) ;
 
 		led_ring.show();
 		delay(100);
