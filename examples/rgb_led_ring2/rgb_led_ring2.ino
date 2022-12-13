2b9fd4346389cc47a4c34ecb7adf771190f4c295
Lars Möllendorf <lars@moellendorf.eu>
2022-12-13 21:31:49 +0100

    Add another RGB LED ring example

---
+++ examples/rgb_led_ring2/rgb_led_ring2.ino
@@ -0,0 +1,30 @@
+#include "MeAuriga.h"
+
+MeRGBLed led_ring = MeRGBLed(0, 12);
+int i = 0;
+
+void setup()
+{
+	led_ring.setpin(44);
+}
+
+void loop()
+{
+	int led;
+
+	/* 0 <= led <= 11 */
+	led = (i % 12);
+	/* 1 <= led <= 12 */
+	led++;
+	led_ring.setColor(0, 0, 0, 0);
+	led_ring.setColor(led, 0, 0, 25);
+	led = (led % 12);
+	led++;
+	led_ring.setColor(led, 0, 0, 255);
+	led = (led % 12);
+	led++;
+	led_ring.setColor(led, 0, 0, 25);
+	led_ring.show();
+	delay(100);
+	i++;
+}
