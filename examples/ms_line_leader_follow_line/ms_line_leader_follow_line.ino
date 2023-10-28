4dfa64ca11e8aadf13403b366bffdddc798dd44c
Lars Möllendorf <lars@moellendorf.eu>
2023-10-28 20:27:04 +0200

    Add example showing how to follow a line using Mindsensors Line Leader

---
+++ examples/ms_line_leader_follow_line/ms_line_leader_follow_line.ino
@@ -0,0 +1,85 @@
+/**
+ * In this example the "Average" value of the Mindsensors Line Leader is used
+ * to calculate a correction value for motor speeds to keep the robot on the
+ * line.
+ *
+ * Edge cases are Average values of
+ *
+ * 10 -> Line Leader is at RIGHT edge of the line
+ * 80 -> Line Leader is at LEFT edge of the line
+ *
+ * The ideal Average value is:
+ *
+ * 45 -> Line Leader is in the MIDDLE of the line
+ *
+ * If average is 80 the robot must turn right. To do so it must
+ *
+ * -> INCREASE the speed of the LEFT motor and/or
+ * -> DECREASE the speed of the RIGHT motor.
+ *
+ * If average is 10 the robot must turn left. To do so it must
+
+ * -> DECREASE the speed of the LEFT motor and/or
+ * -> INCREASE the speed of the RIGHT motor.
+ *
+ * Calculate a deviation of the ideal value of 45 using the Average (avg):
+ *
+ * deviation = 45 - avg
+ *
+ * For 80 it is -> 45 - 80 = -35
+ * For 10 it is -> 45 - 10 =  35
+ * For 45 it is -> 45 - 45 =   0
+ *
+ * Add these results to the right motor speed, and substract it from the
+ * left motor speed and you will get the desired results.
+ */
+#include <MeAuriga.h>
+#include <LmMsLineLeaderV2.h>
+#include <LmEncoderOnBoardMotor.h>
+
+EncoderOnBoardMotor motor_l = EncoderOnBoardMotor(SLOT2);
+EncoderOnBoardMotor motor_r = EncoderOnBoardMotor(SLOT1);
+MsLineLeaderV2 ll = MsLineLeaderV2();
+
+/* "average" value returned by the sensor */
+int avg;
+/* last valid "average" value */
+int last = 45;
+/* normal forward speed */
+int normspeed = 100;
+/**
+ * factor aplied to deviation value to make motor speed at most
+ * - decrease to 0
+ * - increase to 200
+ */
+int fact = normspeed / 35;
+
+void setup() {
+}
+
+void loop() {
+  float deviation;
+
+  avg = ll.getAverage();
+
+  if (avg != 0) {
+    /* if avg is valid, safe it for bad times */
+    last = avg;
+  } else {
+    /**
+     * if avg is 0 we lost the line so best option is to keep on going with the
+     * last valid value we got
+     */
+    avg = last;
+  }
+
+  /* See top comment for details */
+  deviation = 45 - avg;
+  /**
+	 * In case the curves of the line are too steep, multiply the deviation
+	 * by a constant factor
+	 */
+  deviation *= fact;
+  motor_r.forward(-(normspeed + deviation));
+  motor_l.forward(normspeed - deviation);
+}
